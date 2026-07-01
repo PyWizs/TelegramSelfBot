d15a9077fefd16ba171833bd6d140641ffea9df0
PyWiz <121675113+PyWizs@users.noreply.github.com>
2026-07-02 02:30:24 +0330

    0.1

--- handlers/animations.py
+++ handlers/animations.py
@@ -1,21 +1,55 @@
 import asyncio
+import re
 
 from data.animations import ANIMATIONS
 from utils.telegram import auto_delete
 
 
-async def run_animation(update, animation):
-    for a in animation:
-        await update.edit(a["text"])
-        await asyncio.sleep(a["time"])
+def get_run(text: str):
+    match = re.search(r"--run\s+(\d+)", text)
+    return int(match.group(1)) if match else None
 
-    await auto_delete(update, 5)
+
+def get_message(text: str):
+    parts = text.split(maxsplit=1)
+    if len(parts) < 2:
+        return ""
+
+    message = parts[1]
+
+    message = re.sub(r"\s*--run\s+\d+", "", message).strip()
+
+    if "<a>" not in message:
+        message += " <a>" if message else "<a>"
+
+    return message
+
+
+async def run_animation(update, animation: dict, txt: str):
+    message = get_message(txt)
+
+    run = get_run(txt)
+    if run is None:
+        run = animation["run"]
+
+    for _ in range(run):
+        for frame in animation["animation"]:
+            try: await update.edit(message.replace("<a>", frame["text"]))
+            except: break
+
+            await asyncio.sleep(frame["time"])
+
+    if message == "<a>":
+        await auto_delete(update, 5)
 
 
 async def animation_handler(client, update):
-    command = update.text.lower().lstrip("/")
+    if not update.text:
+        return
+
+    command = update.text.split()[0].lstrip("/").lower()
 
     for commands, animation in ANIMATIONS.items():
         if command in commands:
-            await run_animation(update, animation)
+            await run_animation(update, animation, update.text)
             return
